e7f87082baacc5c36d2b91d2a4f1a0f23d59a8e9
Heda Tankeu <hedayvan@gmail.com>
2023-07-17 22:55:41 +0200

    api_view from rest_framework

--- mynotes/api/views.py
+++ mynotes/api/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 # Create your views here.
 
-
+@api_view(['GET'])
 def getRoutes(request):
 
     routes = [
@@ -39,4 +40,4 @@ def getRoutes(request):
         },
     ]
 
-    return Response(routes, safe=False)
+    return Response(routes)

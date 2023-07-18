2085d1e74876a15446265bfa7ca9d791568f2a07
Heda Tankeu <hedayvan@gmail.com>
2023-07-18 02:01:01 +0200

    corsHeaders install to work on two different ports

--- frontend/src/pages/NotesListPages.js
+++ frontend/src/pages/NotesListPages.js
@@ -11,14 +11,17 @@ const NotesListPages = () => {
     let getNotes = async()=>{
         let response = await fetch('http://127.0.0.1:8000/api/notes/')
         let data = await response.json()
-        console.log('DATA: ',data)
 
         setNotes(data)
     }
 
   return (
     <div>
-      notes
+      <div className='notes-list'>
+        {notes.map((note,index)=>(
+            <h3 key={index}>{note.body}</h3>
+        ))}
+      </div>
     </div>
   )
 }
--- mynotes/mynotes/settings.py
+++ mynotes/mynotes/settings.py
@@ -47,6 +47,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+
+    "corsheaders.middleware.CorsMiddleware",
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -127,3 +130,15 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# for specific origins not all
+# CORS_ALLOWED_ORIGINS = [
+#     "https://example.com",
+#     "https://sub.example.com",
+#     "http://localhost:8080",
+#     "http://127.0.0.1:9000",
+# ]
+
+# to allow all origin
+CORS_ALLOW_ALL_ORIGINS = True
